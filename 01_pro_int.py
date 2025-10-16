a72f00aa18d67ea0f946f2d20dc500d100900513
Rajiv <rajiv@Shikhers-MacBook-Air.local>
2025-10-16 11:42:17 -0400

    chnage in the code

--- 01_pro_int.py
+++ 01_pro_int.py
@@ -1,7 +1,7 @@
 # Interger
 
-black_tea_grams = 14
-ginger_grams = 3
+black_tea_grams = 20
+ginger_grams = 311
 
 total_grams = black_tea_grams + ginger_grams
 print(f"Total grams of base tea is {total_grams}")
